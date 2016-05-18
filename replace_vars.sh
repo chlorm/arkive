e8b88457c0fd4c4e79edf68b8b41ff69c01ccfca
Cody W. Opel <codyopel@gmail.com>
2016-05-17 20:37:22 -0400

    fix more file missing passing

--- replace_vars.sh
+++ replace_vars.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-sed -i src/bin/arkive \
-  -e 's/@LIB_BASH_PATH@/lib-bash/'
+if [ "${1}" == '--revert' ] ; then
+  sed -i src/bin/arkive \
+    -e 's/(lib-bash)/(@LIB_BASH_PATH@)/'
 
-sed -i src/share/lib-arkive/ffprobe.bash \
-  -e 's/@FFPROBE_PATH@/ffprobe/'
+  sed -i src/share/lib-arkive/ffprobe.bash \
+    -e 's/ffprobe \\/@FFPROBE_PATH@ \\/'
 
-sed -i src/share/lib-arkive/arkive.bash \
-  -e 's/@FFMPEG_PATH@/ffmpeg/'
+  sed -i src/share/lib-arkive/arkive.bash \
+    -e 's/ffmpeg \\/@FFMPEG_PATH@ \\/'
+else
+  sed -i src/bin/arkive \
+    -e 's/@LIB_BASH_PATH@/lib-bash/'
+
+  sed -i src/share/lib-arkive/ffprobe.bash \
+    -e 's/@FFPROBE_PATH@/ffprobe/'
+
+  sed -i src/share/lib-arkive/arkive.bash \
+    -e 's/@FFMPEG_PATH@/ffmpeg/'
+fi
