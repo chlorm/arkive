0d2de99dc1a4e23a6e0b236aa6572fac15342c05
Cody W. Opel <codyopel@gmail.com>
2016-05-17 20:43:19 -0400

    move FFmpeg::Audio & FFmpeg::Video out on bin/arkive

---
+++ src/share/lib-arkive/ffmpeg/video/main.bash
@@ -0,0 +1,48 @@
+# Copyright (c) 2013-2016, Cody Opel <codyopel@gmail.com>
+# All Rights Reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# WARNING:
+# This mock-up implementation in shell is for testing and demonstration
+# purposes only.
+
+function FFmpeg::Video {
+  local File="${2}"
+  local Stream="${1}"
+  local VideoFilters
+  local VideoBitrate
+  local VideoCodec
+  local VideoPixelFormat
+
+  VideoFilters="$(FFmpeg::Video.filters "${Stream}" "${File}")"
+  VideoBitrate="$(FFmpeg::Video.bitrate "${Stream}" "${File}")"
+  VideoCodec="$(FFmpeg::Video.codec "${Stream}" "${File}")"
+  VideoPixelFormat="$(FFmpeg::Video.pixel_format "${Stream}" "${File}")"
+
+  echo "${VideoBitrate} ${VideoCodec} ${VideoFilters} ${VideoPixelFormat}"
+}
