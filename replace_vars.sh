1dfcf5476c22626c14906391b7dd2ed6c327cb0b
Cody W. Opel <codyopel@gmail.com>
2016-07-17 03:28:20 -0400

    replace_vars.sh: fix after changes in ffprobe

--- replace_vars.sh
+++ replace_vars.sh
@@ -5,12 +5,12 @@ if [ "${1}" == '--revert' ] ; then
     -e 's/source "$(.*)"/source "$(@LIB_BASH_PATH@)"/'
 
   sed -i src/share/lib-arkive/ffprobe.bash \
-    -e 's/ffprobe \\/@FFPROBE_PATH@ \\/'
+    -e 's/ffprobe /@FFPROBE_PATH@ /'
 
   sed -i src/share/lib-arkive/arkive.bash \
     -e 's/ffmpeg ${FFmpegArgsList}/@FFMPEG_PATH@ ${FFmpegArgsList}/'
 else
-  if [ "${1}" == '--local-lib-bash' ] ; then
+  if [ "${1}" == '--local' ] ; then
     file="$(readlink -f "$(pwd)/../lib-bash/src/bin/lib-bash")"
     sed -i src/bin/arkive \
       -e "s,\@LIB_BASH_PATH\@,$file,"
