1f87c24d631dea3cffb97ad8084e17949c333062
Cody W. Opel <codyopel@gmail.com>
2017-03-24 20:40:49 -0400

    FFmpeg::Video.codec:x264_params: update params

--- src/share/lib-arkive/ffmpeg/video/enc-params/x264.bash
+++ src/share/lib-arkive/ffmpeg/video/enc-params/x264.bash
@@ -53,10 +53,9 @@ function FFmpeg::Video.codec:x264_params {
         bc -l |
         xargs printf "%1.0f"
   )"
-  if [ ${FrameRate} -gt 500 ] ; then
+  RcLookahead=${FrameRate}
+  if [ ${RcLookahead} -gt 250 ] ; then
     RcLookahead=250
-  else
-    RcLookahead=${FrameRate}
   fi
 
   MeRange="$(FFmpeg::Video.motion_estimation_range "${Stream}" "${File}")"
@@ -74,13 +73,13 @@ function FFmpeg::Video.codec:x264_params {
   )
   __parameters+=(
     'scenecut=0'
-    'intra-refresh=false'
+    'intra-refresh=0'
     'bframes=3'
     'b-adapt=0'
     'b-bias=0'
     'b-pyramid=normal'
-    'open-gop=false'
-    'cabac=true'
+    'open-gop=0'
+    'cabac=1'
     'ref=3'
     'deblock=-6\:-6'
     'slices=0'
@@ -88,19 +87,17 @@ function FFmpeg::Video.codec:x264_params {
     'slice-max-size=0'
     'slice-max-mbs=0'
     'slice-min-mbs=0'
-    'tff=false'
-    'bff=false'
-    'constrained-intra=true'
+    'tff=0'
+    'bff=0'
+    'constrained-intra=1'
     #'pulldown=none'
-    'fake-interlaced=false'
-    'interlaced=false'
+    'fake-interlaced=0'
+    'interlaced=0'
     # TODO: support alternative frame-packing for 3D sources
     'frame-packing=6'
-  )
-  __parameters+=("rc-lookahead=${RcLookahead}")
-  __parameters+=("vbv-maxrate=${BufSize}")
-  __parameters+=("vbv-bufsize=${BufSize}")
-  __parameters+=(
+    "rc-lookahead=${RcLookahead}"
+    "vbv-maxrate=${BufSize}"
+    "vbv-bufsize=${BufSize}"
     'vbv-init=0.9'
     'qpmin=10'
     'qpmax=51' # XXX
@@ -114,34 +111,33 @@ function FFmpeg::Video.codec:x264_params {
     'chroma-qp-offset=0'
     'aq-mode=2'
     'aq-strength=0.3'
-    'mbtree=true'
+    'mbtree=1'
     'qcomp=0.9'
-    # Does nothing when mbtree is enabled
-    'cplxblur=0'
+    'cplxblur=0'  # Does nothing when mbtree is enabled
     'qblur=0'
     #'qpfile'
     'partitions=all\:i8x8'
     'direct=auto'
-    'weightb=true'
+    'weightb=1'
     'weightp=2'
     'me=umh'
     "merange=${MeRange}"
     'subme=10'
     'psy-rd=0.0\:0.7' # FIXME
-    'psy=true'
-    'mixed-refs=true'
-    'chroma-me=true'
-    '8x8dct=true'
+    'psy=1'
+    'mixed-refs=1'
+    'chroma-me=1'
+    '8x8dct=1'
     'trellis=2'
-    'fast-pskip=false'
-    'dct-decimate=false'
+    'fast-pskip=0'
+    'dct-decimate=0'
     'nr=0'
     'deadzone-inter=21'
     'deadzone-intra=11'
-    "cqmfile=${ARKIVE_LIB_DIR}/ffmpeg/video/enc-params/cqm-matrices/prestige_matrix"
+    #"cqmfile=${ARKIVE_LIB_DIR}/ffmpeg/video/enc-params/cqm-matrices/prestige_matrix"
     #'overscan'
     #'videoformat'
-    ###'range=tv'
+    ###'range=pc'
     'colorprim=bt709'
     'transfer=bt709'
     'colormatrix=bt709'
@@ -160,32 +156,32 @@ function FFmpeg::Video.codec:x264_params {
     "level=$(FFmpeg::Video.level:h264 "${Stream}" "${File}")"
   )
   __parameters+=(
-    'bluray-compat=false'
-    'avcintra-class=false'
-    'stitchable=true'
+    'bluray-compat=0'
+    ###'avcintra-class=0'
+    'stitchable=0'
     ###'log-level=info'
-    'psnr=false'
-    'ssim=false'
+    'psnr=0'
+    'ssim=0'
   )
   __parameters+=("threads=$(Cpu::Logical)")
   __parameters+=(
     'lookahead-threads=1'
-    'sliced-threads=false'
+    'sliced-threads=0'
     #'sync-lookahead'
-    'non-deterministic=false'
-    'cpu-independent=false'
+    'non-deterministic=0'
+    'cpu-independent=0'
     'asm=auto'
-    'opencl=false'
-    'opencl-clbin=false'
+    'opencl=0'
+    'opencl-clbin=0'
     #'opencl-device'
     #'dump-yuv'
     #'sps-id'
-    #'aud=false'
+    #'aud=0'
     #'force-cfr'
     #'tcfile-in'
     #'tcfile-out'
     #'timebase'
-    ######'dts-compress=false'
+    ######'dts-compress=0'
   )
 
   if [ ${FFMPEG_VIDEO_ENCODER_PASSES} -gt 1 ] ; then
